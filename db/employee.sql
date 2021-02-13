ea82d3649be6232142d9f75f750d498e2b654fc5
ccsxw6 <ccsxw6@gmail.com>
2021-02-13 16:46:12 -0600

    changed server.js, added seed sql

--- db/employee.sql
+++ db/employee.sql
@@ -1,41 +1,34 @@
-DROP DATABASE IF EXISTS employee_DB;
-CREATE DATABASE employee_DB;
+DROP DATABASE IF EXISTS employee_trackerDB;
 
-USE employee_DB;
+CREATE DATABASE employee_trackerDB;
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL, 
-  PRIMARY KEY (id)
+USE employee_trackerDB;
+
+CREATE TABLE department
+(
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE employee_role (
+CREATE TABLE role
+(
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL, 
-  salary DECIMAL(10, 4) NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
+  title VARCHAR(30),
+  salary DECIMAL (9,2),
+  department_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30), 
+CREATE TABLE employee
+(
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT NOT NULL, 
-  manager_id INT NULL, 
-  PRIMARY KEY (id)
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
-
---Department Table Information
-INSERT INTO department (department_name) VALUES ('Marketing');
-INSERT INTO department (department_name) VALUES ('Human Resources');
-INSERT INTO department (department_name) VALUES ('Accounting');
-
---Employee Role Table Information
-INSERT INTO employee_role (title, salary, department_id) VALUES ('Manager', '100,000', '1');
-INSERT INTO employee_role (title, salary, department_id) VALUES ('Employee', '50,000', '2');
-
-
---Employee Personal Table Information
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Chris', 'Stanfill', '20', '30');
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('The', 'Donald', '21', '30');
---
+++ server.js
@@ -0,0 +1,230 @@
+// Dependencies
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "employee_trackerDB",
+});
+
+connection.connect((err) => {
+	if (err) throw err;
+});
+
+
+const promptUser = () => {
+  inquirer.prompt({
+      type: "list",
+      name: "question",
+      message: "What would you like to do?",
+      choices: [
+        "view all employees",
+        "view all roles",
+        "view all departments",
+        "add employee",
+        "add department",
+        "add role",
+        "update employee role",
+        "remove employee"
+      ]
+    }).then((answer) => {
+      console.log(answer);
+      switch (answer.question) {
+        case "view all employees":
+          viewallEmployeesloyees();
+          break;
+
+        case "view all roles":
+          viewallroles();
+          break;
+
+        case "view all departments":
+          viewalldepartments();
+          break;
+
+        case "add employee":
+          addEmployee();
+          break;
+
+        case "update employee role":
+          updateEmpRole();
+          break;
+
+        case "add department":
+          addDepartment();
+          break;
+
+        case "add role":
+          addRole();
+          break;
+      }
+    });
+};
+promptUser();
+
+// allows user to view all departments currently in the database
+const viewalldepartments = () => {
+  connection.query("SELECT * FROM department", (err, answer) => {
+    console.table(answer);
+  });
+  promptUser();
+}
+
+// allows user to view all employee roles currently in the database
+const viewallroles = () => {
+  connection.query("SELECT * FROM role", (err, answer) => {
+    console.table(answer);
+  });
+  promptUser();
+}
+
+// allows user to view all employees currently in the database
+const viewallEmployeesloyees = () => {
+  console.log("retrieving employess from database");
+  var fancyQuery =
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id;";
+  connection.query(fancyQuery, (err, answer) => {
+    console.table(answer);
+  });
+  promptUser();
+}
+
+// allows user to add a new employee to database
+const addEmployee = () =>  {
+  inquirer.prompt([
+      {
+        type: "input",
+        message: "Enter employee first name",
+        name: "firstName"
+      },
+      {
+        type: "input",
+        message: "Enter employee last name",
+        name: "lastName"
+      }
+    ]).then((answer) => {
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: null,
+          manager_id: null
+        },
+        (err, answer) => {
+          if (err) {
+            throw err;
+          }
+          console.table(answer);
+        }
+      );
+      promptUser();
+    });
+}
+
+
+const updateEmpRole = () =>  {
+  let allEmployees = [];
+  connection.query("SELECT * FROM employee", function(err, answer) {
+    for (let i = 0; i < answer.length; i++) {
+      let stringOfEmployees =
+        answer[i].id + " " + answer[i].first_name + " " + answer[i].last_name;
+      allEmployees.push(stringOfEmployees);
+    }
+    inquirer.prompt([
+        {
+          type: "list",
+          name: "udpateRole",
+          message: "select employee to update role",
+          choices: allEmployees
+        },
+        {
+          type: "list",
+          message: "select new role",
+          choices: ["manager", "employee"],
+          name: "newrole"
+        }
+      ]).then((answer) =>  {
+        console.log("about to update", answer);
+        const idToUpdate = {};
+        idToUpdate.employeeId = parseInt(answer.udpateRole.split(" ")[0]);
+        if (answer.newrole === "manager") {
+          idToUpdate.role_id = 1;
+        } else if (answer.newrole === "employee") {
+          idToUpdate.role_id = 2;
+        }
+        connection.query(
+          "UPDATE employee SET role_id = ? WHERE id = ?",
+          [idToUpdate.role_id, idToUpdate.employeeId],
+          (err, data) => {
+            promptUser();
+          }
+        );
+      });
+  });
+}
+
+
+const addDepartment = () =>  {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "enter department name",
+      name: "department"
+    }).then((answer) =>  {
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          name: answer.department
+        },
+        (err, answer) => {
+          if (err) {
+            throw err;
+          }
+        }
+      ),
+        console.table(answer);
+      promptUser();
+    });
+}
+
+
+const addRole = () =>  {
+  inquirer.prompt([
+      {
+        type: "input",
+        message: "enter employee title",
+        name: "addtitle"
+      },
+      {
+        type: "input",
+        message: "enter employee salary",
+        name: "addsalary"
+      },
+      {
+        type: "input",
+        message: "enter employee department id",
+        name: "addDepId"
+      }
+    ]).then((answer) =>  {
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          title: answer.addtitle,
+          salary: answer.addsalary,
+          department_id: answer.addDepId
+        },
+        (err, answer) => {
+          if (err) {
+            throw err;
+          }
+          console.table(answer);
+        }
+      );
+      promptUser();
+    });
+}
